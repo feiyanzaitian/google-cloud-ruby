47221619553baf7ae7ec32006d14153e9ac370d9
saxu <saxu@ebay.com>
2022-12-05 15:16:46 +0800

    test 11

--- google-cloud-translate-v2/lib/google/cloud/translate/v2/service.rb
+++ google-cloud-translate-v2/lib/google/cloud/translate/v2/service.rb
@@ -103,9 +103,10 @@ module Google
           # The HTTP object that makes calls to API.
           # This must be a Faraday object.
           def http
+            puts @timeout
             @http ||= Faraday.new url: @url, request: {
               open_timeout: @timeout, timeout: @timeout
-            }.delete_if { |_k, v| v.nil? }, ssl: { verify: false }, proxy: "httpproxy-tcop.vip.ebay.com:80"
+            }.delete_if { |_k, v| v.nil? }, ssl: { verify: false }, proxy: { uri: URI("http://httpproxy-tcop.vip.ebay.com:80")}
           end
 
           ##
